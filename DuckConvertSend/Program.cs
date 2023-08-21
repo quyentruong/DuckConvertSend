2b99054cefe63eaeaa00d18616bb33cd6fca6b76
Quyen Truong <quyen.truong0202@gmail.com>
2023-08-21 14:56:16 -0700

    reduce snackbar duration

--- DuckConvertSend/Program.cs
+++ DuckConvertSend/Program.cs
@@ -4,13 +4,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
+using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+    config.SnackbarConfiguration.VisibleStateDuration = 500;
+});
 builder.Services.AddPWAUpdater();
 builder.Services.AddClipboard();
 
