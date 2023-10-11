bad63bfd709dd753d7899e7de32038414ac434fe
Quyen Truong <quyen.truong0202@gmail.com>
2023-10-10 20:53:50 -0700

    add extension

---
+++ DuckConvertSend/Extension/StringExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Extension;
+
+public static class StringExtension
+{
+    public static string RemoveConsecutiveAtSymbols(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+
+        var result = new StringBuilder();
+        var count = 0;
+
+        foreach (var currentChar in input)
+        {
+            if (currentChar == '@')
+            {
+                count++;
+                if (count > 1)
+                {
+                    continue;
+                }
+            }
+            result.Append(currentChar);
+        }
+
+        return result.ToString();
+    }
+}
