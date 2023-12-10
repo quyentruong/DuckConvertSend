cdabbd33c7547824677966cc189ea103c8a3444f
Quyen Truong <quyen.truong0202@gmail.com>
2023-12-09 23:59:08 -0800

    Add custom validation

---
+++ DuckConvertSend/Attributes/DuckEmailValidationAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DuckConvertSend.Attributes;
+
+public sealed class DuckEmailValidationAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is string emailAddress)
+        {
+            // Perform the existing validation check
+            var emailValidator = new EmailAddressAttribute();
+            if (!emailValidator.IsValid(emailAddress))
+            {
+                return false;
+            }
+
+            // Perform the additional validation check
+            if (!emailAddress.Contains("@duck.com"))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
